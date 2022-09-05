7609a1c7126d106960a5522336154ea2efe8bc18
GunjonRoy <01gunjonroy5722@gmail.com>
2022-09-05 23:20:24 +0600

    complete

--- lib/cheapcitybd_webview_page.dart
+++ lib/cheapcitybd_webview_page.dart
@@ -12,10 +12,10 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-void main() => runApp(const MaterialApp(home: CheapCityBdWebViewPage()));
 
 const String kNavigationExamplePage = '''
 <!DOCTYPE html><html>
@@ -101,8 +101,23 @@ class _CheapCityBdWebViewPageState extends State<CheapCityBdWebViewPage> {
   void initState() {
     super.initState();
     if (Platform.isAndroid) {
+      initialization();
       WebView.platform = SurfaceAndroidWebView();
     }
+  }
+  void initialization() async {
+    // This is where you can initialize the resources needed by your app while
+    // the splash screen is displayed.  Remove the following example because
+    // delaying the user experience is a bad design practice!
+    // ignore_for_file: avoid_print
+    print('ready in 3...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 2...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('ready in 1...');
+    await Future.delayed(const Duration(seconds: 1));
+    print('go!');
+    FlutterNativeSplash.remove();
   }
 
   Future<bool> _willPopCallback() async {
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:cheapcitybd/cheapcitybd_webview_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
